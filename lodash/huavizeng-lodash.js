8ec829fb0506c55586a8e661aa85e43f17d51c13
zenghuangwei <zenghuangweimaya@qq.com>
2019-01-23 18:22:48 +0800

    89999

--- lodash/huavizeng-lodash.js
+++ lodash/huavizeng-lodash.js
@@ -1,7 +1,7 @@
-var USERNAME = {
+var huaxizeng = {
   chunk: function chunk(ary, size = 1) {
     var result = []
-    for(var i = 0; i< ??; i++) {
+    for(var i = 0; i< ary.length; i++) {
       result.push(ary.slice(i, i + size))
     }
     return result
@@ -9,10 +9,32 @@ var USERNAME = {
   compact: function(ary) {
     var result = []
     for(var item of ary) {
-      if (item) {
+      if (item!=false) {
         result.push(item)
       }
     }
     return result
+  },
+  comcat: function(ary,value){
+    if (typeof(value)=='object') {
+     for (var i = 0; i < value.length; i++) {
+       ary.push(value[i])
+
+     }
+     return ary
+    } else if (typeof(value)=='number') {
+      return ary.push(value)
+    }
   }
+  reverse:function(array){
+    var temp
+    var len=array.length
+    var mid=Math.floor(len/2)
+    for (var i =len-1 ; i >mid; i--) {
+      temp=array[len-i-1]
+      array[len-i-1]=array[i]
+      array[i]=temp
+    }
+    return array
+  }abcdefabcdef
 }
